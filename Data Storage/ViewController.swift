cd8fb44e3f6d90483db76c335e541e65a145fa03
abdulrahmanafa <master197723@gmail.com>
2020-04-05 14:43:26 +0300

    Initial Commit

---
+++ Data Storage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Data Storage
+//
+//  Created by Abdulrhman A F  Alomairi on 4/5/20.
+//  Copyright © 2020 Abdulrhman A F  Alomairi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
